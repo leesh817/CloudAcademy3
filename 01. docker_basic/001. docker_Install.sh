fffe933943d8f86ae0750cc318a6a379ac902e3e
leesh817 <leeseonhyo817@gmail.com>
2020-07-25 01:21:54 +0900

    test

---
+++ 01. docker_basic/001. docker_Install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo http://download.docker.com/linux/centos/docker-ce.repo
+yum repolist
+sudo yum install -y docker-ce docker-ce-cli(client 도구) containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker <User ID>
