a23b09d2bab98bd7baaafee85e6a0a56aa422102
leesh817 <leeseonhyo817@gmail.com>
2020-07-25 02:02:15 +0900

    Docker Container Network Setting

---
+++ 01. docker_basic/014. docker_Network.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker network ls
+// bridge, none, host
+docker network create --subnet 172.18.0.0/16 --gateway 172.18.0.1 --driver bridge net1
+// create network
+docker container run -d --name os9 --network net1 centos:latest
+// create os9 container with 'net1' network
+docker contaienr run -it --name os19 --network none centos:latest
+// create os19 container with 'none' network
