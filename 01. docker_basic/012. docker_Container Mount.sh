765cf0b8b843e6514576aab5a3bb4e8da2c27ac7
leesh817 <leeseonhyo817@gmail.com>
2020-07-25 02:00:39 +0900

    Docker Container File Mount

---
+++ 01. docker_basic/012. docker_Container Mount.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p /var/www/html
+echo "Hello world!" > /var/www/html/index.html
+// create directory and 'index.html' file
+docker container run -d --name web5 -v '/var/www/html/index.html:/usr/local/apache2/htdocs/index.html' httpd:latest
+// mounted 'index.html'
+docker container run -d --name web10 -v '/var/www/html/index.html:/usr/local/apache2/htdocs/index.html:ro' httpd:latest
+// mounted 'index.html' as read-only
+
+mkdir contents
+docker container run -itd --name os10 -v '/home/student/contents:/contents' centos:latest
+// mounted 'contents' directory
