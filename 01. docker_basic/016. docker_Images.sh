320955b48c074ceb049734a2d4f5ca5e4b99d450
leesh817 <leeseonhyo817@gmail.com>
2020-07-25 02:03:40 +0900

    Docker Container Archive and Image Tag

---
+++ 01. docker_basic/016. docker_Images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+// Image Archive
+docker save -o test.tar centos:latest httpd:latest
+docker load -i test.tar
+
+// Image Tag
+docker tag httpd:latest httpd:v1
+
+// Container make Image
+docker run -it --name os-web1 centos:latest
+// yum install -y httpd
+docker commit -c 'CMD["httpd","-D","FOREGROUND"]' os-web1 centos-web:v1
+
+// Container export, import
+docker export -o centos-web.tar os-web1
+docker import -i centos-web.tar abc:latest
