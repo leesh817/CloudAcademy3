ca355896dc27bcbe55560d3111573ddb8d4e306f
leesh817 <leeseonhyo817@gmail.com>
2020-07-25 02:01:08 +0900

    Docker Container Volume Mount

---
+++ 01. docker_basic/013. docker_Container Volume.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker volume create vol1
+// volume create
+docker run -d --name db5 -v 'vol1:/var/lib/mysql' -e "MYSQL_ROOT_PASSWORD=1234" mysql:5.7
+// mounted vol1
+docker run -itd --name os0 -v 'vol2:/' centos:latest
+// mounted without creating vol2
